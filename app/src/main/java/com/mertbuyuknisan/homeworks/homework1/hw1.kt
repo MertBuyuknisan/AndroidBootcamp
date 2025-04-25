ffeb9afb2abe3818513ccd73ce0c1acb90564c7a
MertBuyuknisan <136019582+MertBuyuknisan@users.noreply.github.com>
2025-04-26 01:35:13 +0300

    Initial commit

---
+++ app/src/main/java/com/mertbuyuknisan/homeworks/homework1/hw1.kt
@@ -0,0 +1,100 @@
+package com.mertbuyuknisan.homeworks.homework1
+
+
+fun main(){
+    val city="Adana"
+    println("City: $city")
+    var country="Turkey"
+    println("Country: $country")
+    val phoneNumber="05xxxxxxxxx"
+    println("Phone Number: $phoneNumber")
+    val postalCode="01000"
+    println("Postal Code: $postalCode")
+    var email:String="example@gmail.com"
+    println("Email: $email")
+    val profession="Computer Engineer"
+    println("Profession $profession")
+    var stockAmount:Int=300
+    println("Stock Amount: $stockAmount")
+    val customerName="Mert"
+    println("Customer Name: $customerName")
+    var balance:Int=100000000
+    println("Balance: $balance")
+    val birthDate="0X-0X-1XXX"
+    println("Birth Date: $birthDate")
+    var salary=120000
+    println("Salary: $salary")
+    var maritalStatus="Single"
+    println("Marital Status: $maritalStatus")
+    var productReview="This product is very effective."
+    println("Product Review: $productReview")
+    var paymentDate="25.04.2025"
+    println("Payment Date: $paymentDate")
+    val pay:Double=293.99
+    println("Pay: $pay")
+    val countOfOrder=1000
+    println("Count of Order: $countOfOrder")
+    var carModel="Ford Mustang"
+    println("Car Model $carModel")
+    val bookName="Sefiller"
+    println("Book Name: $bookName")
+    val publicationDate="12.10.1980"
+    println("Publication Date: $publicationDate")
+    var discountAmount=200
+    println("DiscountAmount: $discountAmount")
+    var numberOfRooms:Int=20
+    println("Number of Rooms $numberOfRooms")
+    val latitude:Double=41.002
+    println("Latitude: $latitude")
+    val longitude:Double=14.003
+    println("Longitude: $longitude")
+    var productName:String="Computer"
+    println("Product Name: $productName")
+    var foodPrice:Float=192.39f
+    println("Food Price: $foodPrice")
+    val brand:String="Asus"
+    println("Brand: $brand")
+    var songName:String="Mein Herz brennt"
+    println("Song Name: $songName")
+    var videoDuration:Float=3.40f
+    println("Video Duration $videoDuration")
+    val productRating=4.30
+    println("Product Rating: $productRating")
+    var imageName:String="images.png"
+    println("Image Name: $imageName")
+    var fileFormat:String="txt"
+    println("File Format: $fileFormat")
+    val color="Black"
+    println("Color: $color")
+    var colorCode:String="#000000"
+    println("Color Code: $colorCode")
+    val phoneModel="Samsung S25"
+    println("Phone Model: $phoneModel")
+    val screenSize=6.4
+    println("Screen Size: ${screenSize.toString()}")
+    var weight:Double=3.4
+    println("Weight: $weight")
+    val nationalDay:String="29 Ekim"
+    println("National Day $nationalDay")
+    val holidayName:String="Cumartesi"
+    println("Holiday Name: $holidayName")
+    var reservationDate:String="12.03.2022"
+    println("Reservation Name: $reservationDate")
+    val streetName="Cumhuriyet Sokak"
+    println("Street Name: $streetName")
+    var busLine:Int=302
+    println("Bus Line: $busLine")
+    var remainingMinutes:Int=54
+    println("Remaining Minutes: $remainingMinutes")
+    val trackingCode:String="3823283283"
+    println("Tracking Code: $trackingCode")
+    var couponDuration:String="12.01.2012"
+    println("Coupon Duration $couponDuration")
+    val couponCode="PZR2020"
+    println("Coupon Code: $couponCode")
+    val invoiceAddress:String ="X mah 282348 sk"
+    println("Invoice Address: $invoiceAddress")
+
+
+
+}
